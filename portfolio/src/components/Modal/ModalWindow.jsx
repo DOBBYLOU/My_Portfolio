fea84c196484b838cc6c3163971d846fabe0b013
AndrianovSasha <Gn8282557@gmail.com>
2026-04-12 12:14:46 +0300

    fix window bag

--- portfolio/src/components/Modal/ModalWindow.jsx
+++ portfolio/src/components/Modal/ModalWindow.jsx
@@ -1,9 +1,9 @@
 import Styles from "./ModalWindow.module.css"
 
-function ModalWindow({ active, setActive, autoWidth, children }) {
+function ModalWindow({ active, setActive, children }) {
     return (
         <div className={`${Styles.back} ${active ? Styles.active : ""}`} onClick={() => setActive(false)} >
-            <div className={`${Styles.modal} ${autoWidth ? Styles.autoWidth : ""}`} onClick={(e) => e.stopPropagation()}>{children}</div>
+            <div className={`${Styles.modal} `} onClick={(e) => e.stopPropagation()}>{children}</div>
         </div >
     )
 }
--- portfolio/src/components/Portfolio/Portfolio.jsx
+++ portfolio/src/components/Portfolio/Portfolio.jsx
@@ -12,7 +12,7 @@ function HoneyText() {
 
 function HoneyAbout() {
     return (
-        <div className={`${Styles.about} ${Styles.aboutText}`}>
+        <div className={`${Styles.aboutText}`}>
             <h4>My first project</h4>
             <p>This is my first project. Although it is still in an early stage and not fully responsive, it allowed me to gain valuable experience. I spent approximately two months working on it, and upon completion, despite the numerous mistakes it contains, I developed a strong understanding of the importance of proper code naming, effective styling practices, and maintaining high standards as a developer.</p>
 
@@ -30,7 +30,7 @@ function VelocityText() {
 
 function VelocityAbout() {
     return (
-        <div className={`${Styles.about} ${Styles.aboutText}`}>
+        <div className={`${Styles.aboutText}`}>
             <h4>Structure + Js</h4>
             <p> This website was developed after I studied the BEM (Block–Element–Modifier) methodology, which significantly improved my approach to structuring styles and markup. The codebase became cleaner and more maintainable, existing bugs were resolved, and JavaScript logic was successfully implemented.
                 Through this project, I moved beyond simply understanding how websites are built to writing code that is clear and maintainable for other developers. It was also during this project that I began working with version control using Git and GitHub for the first time.</p>
@@ -48,7 +48,7 @@ function PortfolioText() {
 
 function PortfolioAbout() {
     return (
-        <div className={`${Styles.about} ${Styles.aboutText}`}>
+        <div className={`${Styles.aboutText}`}>
             <h4>React + Git</h4>
             <p> This is my portfolio website (the one you are currently viewing). Although I have worked with React before, this is the first React site I can showcase for various reasons some of my other projects are lost, and for others, I no longer have access. This site includes routing functionality and is managed using GitHub.
             </p>
